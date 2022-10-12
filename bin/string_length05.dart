cf0236633d667a71bc2a73f86a2dc98e2e1f16b7
timur <ttoshte@gmail.com>
2022-10-12 14:38:29 +0300

    5

--- bin/string_length05.dart
+++ bin/string_length05.dart
@@ -6,7 +6,10 @@
         s2: string
     Returns:
         total length of strings
-*/
+*/int func(String s1,String s2){
+ return s1.length + s2.length;  
+}
 void main() {
+    print(func("code","python"));
   // write your code here
 }
